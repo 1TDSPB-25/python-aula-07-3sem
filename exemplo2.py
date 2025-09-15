1d4e071ad71ee35daf6ca4e0a046b5490bee67a5
Ruan <progmruansilva@gmail.com>
2025-09-15 08:30:08 -0300

    exemplo 2 - primeria parte

---
+++ exemplo2.py
@@ -0,0 +1,23 @@
+# Calculadora simples
+
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+opcao = int(input("Escolha uma opção: "))
+
+if 1 <= opcao <= 4:
+    a = float(input("primeiro número: "))
+    b = float(input("segundo número: "))
+
+if opcao == 1:
+    print(f'Resultado da soma: {a + b}')
+elif opcao == 2:
+    print(f'Resultado da subtração: {a - b}')
+elif opcao == 3:
+    print(f'Resultado da multiplicação: {a * b}')
+elif opcao == 4:
+    print(f'Resultado da divisão: {a / b}')
+else:
+    print("Opção invalida")
